717f3d5a8794100ac8434ac71302a0201cb6f902
Lord Voldemort <lordvoldemort1729@gmail.com>
2013-11-23 16:42:11 +0530

    Intial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Onrails::Application.config.secret_token = 'e44c7f0f1c145c530714b2e2804dcbec3fb204266ef1227c64878d01ea50506f8000bca4d8dfcb082d6b054c0e5f8c32f166d27d54b2bb837a83dd3fd3d264ba'
